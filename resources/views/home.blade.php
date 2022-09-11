68c0100d608624b1f10d3f8656a15907ba3a9666
anonym2800 <svamz34@gmail.com>
2022-09-11 16:29:36 +0700

    perubahannn

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,4 +7,4 @@
         <p>Labore voluptatibus voluptatum architecto saepe impedit. Nobis, sequi a iure totam nihil beatae quos deleniti velit eum ut tempora animi maiores officia, qui vitae natus laudantium similique, rem necessitatibus voluptate? Sed eos quod, in cum error veritatis neque atque nam est,</p>
         <p>reiciendis commodi, delectus quae possimus illo odit illum eum. Temporibus ullam odio facere, vitae suscipit ea incidunt! Delectus, earum autem eaque ad eum sit, repudiandae doloremque adipisci nam placeat fuga laborum eveniet corporis temporibus voluptates! Velit molestiae nihil omnis iure ipsum!</p>   
     </h3>
-@endsection
+@endsectiona
